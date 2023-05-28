fa70d8c38bbc287c92b499bdbe80b0d1a7663aa5
SrtaAbbes <114194164+SrtaAbbes@users.noreply.github.com>
2023-05-28 17:09:59 -0300

    projeto final PW

---
+++ 2023-bsi-pw-main/src/main/java/model/dto/RetornoAutenticacaoDTO.java
@@ -0,0 +1,14 @@
+package model.dto;
+
+public class RetornoAutenticacaoDTO {
+
+    private String retorno;
+
+    public String getRetorno() {
+        return retorno;
+    }
+
+    public void setRetorno(String retorno) {
+        this.retorno = retorno;
+    }
+}
