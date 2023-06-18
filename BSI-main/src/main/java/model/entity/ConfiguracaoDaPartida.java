5cf490b082a201d42f585424cef05afd5a0bc5d2
srtaabbes <vitoriaabbesifg@gmail.com>
2023-06-17 21:49:48 -0300

    log, configPartida

---
+++ BSI-main/src/main/java/model/entity/ConfiguracaoDaPartida.java
@@ -0,0 +1,7 @@
+package model.entity;
+
+public class ConfiguracaoDaPartida {
+    /*
+    Tabela onde é definido a quantidade de jogadores por time, o valor da cesta mínima e máxima e a duracao da partida.
+     */
+}
